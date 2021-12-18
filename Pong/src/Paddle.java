cc59264f54ad725354a6b1c8c0e41d4ab483310e
RJDPayne <richardpayne@myop.ac.nz>
2021-12-19 04:10:09 +1300

    Changed background to white Paddles and ball changed to Black Winning score changed to 11

--- Pong/src/Paddle.java
+++ Pong/src/Paddle.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 
 public class Paddle extends Sprite{
 	private static final int PADDLE_WIDTH = 10, PADDLE_HEIGHT = 100, PADDLE_DISTANCE_FROM_EDGE = 40;
-	private static final Color PADDLE_COLOUR = Color.WHITE;
+	private static final Color PADDLE_COLOUR = Color.BLACK;
 	
 	public Paddle(Player player, int panelWidth, int panelHeight) {
 		setWidth(PADDLE_WIDTH);
